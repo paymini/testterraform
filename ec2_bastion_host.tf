32e0d53ca8152f96d776e4385745be218dcd57f1
tien.nguyen <tien.nguyen@kyanon.digital>
2023-04-24 14:12:39 +0700

    update bastion network

--- ec2_bastion_host.tf
+++ ec2_bastion_host.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
   key_name      = "aws_key"
   subnet_id     = aws_subnet.public_subnet_1.id
-  vpc_security_group_ids = [aws_security_group.main.id]
+  vpc_security_group_ids = [aws_security_group.bastion_security_group.id]
   user_data = "${data.template_file.userdata.rendered}"
   associate_public_ip_address = true
   tags = {
@@ -11,3 +11,16 @@ resource "aws_instance" "app_server" {
   }
 }
 
+
+# Create a security group for the Bastion host
+resource "aws_security_group" "bastion_security_group" {
+  name_prefix = "bastion-sg-"
+  vpc_id      = aws_vpc.example_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
