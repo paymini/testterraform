c33b45bcb59efce587424646da0086150a99e69d
tien.nguyen <tien.nguyen@kyanon.digital>
2023-04-24 14:04:38 +0700

    create bastion  host

---
+++ ec2_bastion_host.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "app_server" {
+  ami           = "ami-0a72af05d27b49ccb"
+  instance_type = "t2.micro"
+  key_name      = "aws_key"
+  subnet_id     = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.main.id]
+  user_data = "${data.template_file.userdata.rendered}"
+  associate_public_ip_address = true
+  tags = {
+    Name = "my-bastion-host"
+  }
+}
+
