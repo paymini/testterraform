9026d811e7aa141de7936f0d0f01b730ab622f25
tien.nguyen <tien.nguyen@kyanon.digital>
2023-04-24 11:41:22 +0700

    update us-east-1a

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "example_vpc" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id = aws_vpc.example_vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id = aws_vpc.example_vpc.id
   cidr_block = "10.0.2.0/24"
-  availability_zone = "us-west-2b"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -32,7 +32,7 @@ resource "aws_subnet" "public_subnet_2" {
 resource "aws_subnet" "private_subnet_1" {
   vpc_id = aws_vpc.example_vpc.id
   cidr_block = "10.0.3.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "private-subnet-1"
@@ -42,7 +42,7 @@ resource "aws_subnet" "private_subnet_1" {
 resource "aws_subnet" "private_subnet_2" {
   vpc_id = aws_vpc.example_vpc.id
   cidr_block = "10.0.4.0/24"
-  availability_zone = "us-west-2b"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "private-subnet-2"
