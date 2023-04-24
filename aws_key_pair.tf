716fdfff6d4252d5838e99ec28db41d4775e5a23
tien.nguyen <tien.nguyen@kyanon.digital>
2023-04-24 15:00:06 +0700

    update key

--- aws_key_pair.tf
+++ aws_key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "localtest" {
   key_name   = "aws_key"
-  public_key = var.aws_key
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6wxGtAobEwYChJWkYiHQqXDhDVam4RMuAtXZkLg8ZqqUrAx0I4f9klHwsaIMPhF7lygOvq+TEEo5SZ+FrMEG9/sHQnWtTGso8AoYna2HGpkXJvcYAA6FU1DgtLuxycwucUjvJchSbPUSKYG47E/iSzD5DH1+D5Rmpp3oEQ5hLDQ07PGGV6ZVjrcrsp/8Jp9TYMeQSI1vA20IsZ6k1cwUMtq9WMfs0bw/h/9GKSPVDd9FYLYFgxB60Perbka/yIoi/qnW9JRBL+NMIX/Rq+KiycwkQjIQEFj9m6fWMrp5UlV78SiEGX/9bLDsj2MMoB/1r10TzDs19qLNC/Xpvo2RxZtVJUHT2ySSSwUmaRebdRyRcQS0ubSvkYksntK4VLynS/YuUTvTWZVIpL6ZwAR/yjGYrmqk+irRnmj4WNtUMUF+BY0t7PxswteyyyogvRo4U7vvbLb9yeqggfyFmEL/TKx7adG5qgvj76eR60sQ9x/Mvr1OvuzRcCMOEGMbF/Q0= archer@DESKTOP-8IESQC1"
 }
