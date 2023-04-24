a8942b30bb6c4e4e37c80901a12be6d2998bb97e
tien.nguyen <tien.nguyen@kyanon.digital>
2023-04-24 13:48:28 +0700

    update

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "aws_key" {}
 variable "region" {
   description = "create in region"
   type = string
-  default = "us-east-1"
+  default = "ap-southeast-1"
 }
 
 # variable "access_key" {
