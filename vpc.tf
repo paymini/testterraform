812705b61565e67e2e0d15539d24c00912313b2d
tien.nguyen <tien.nguyen@kyanon.digital>
2023-04-24 15:21:41 +0700

    dfasdfas

--- vpc.tf
+++ vpc.tf
@@ -1,106 +1,106 @@
-# # Create a VPC with two public and two private subnets across two AZs
-# resource "aws_vpc" "example_vpc" {
-#   cidr_block = "10.0.0.0/16"
+# Create a VPC with two public and two private subnets across two AZs
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
 
-#   tags = {
-#     Name = "example-vpc"
-#   }
-# }
+  tags = {
+    Name = "example-vpc"
+  }
+}
 
-# resource "aws_subnet" "public_subnet_1" {
-#   vpc_id = aws_vpc.example_vpc.id
-#   cidr_block = "10.0.1.0/24"
-#   availability_zone = "ap-southeast-1a"
-#   map_public_ip_on_launch = true
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = aws_vpc.example_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-southeast-1a"
+  map_public_ip_on_launch = true
 
-#   tags = {
-#     Name = "public-subnet-1"
-#   }
-# }
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
 
-# resource "aws_subnet" "public_subnet_2" {
-#   vpc_id = aws_vpc.example_vpc.id
-#   cidr_block = "10.0.2.0/24"
-#   availability_zone = "ap-southeast-1b"
-#   map_public_ip_on_launch = true
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id = aws_vpc.example_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-southeast-1b"
+  map_public_ip_on_launch = true
 
-#   tags = {
-#     Name = "public-subnet-2"
-#   }
-# }
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
 
-# resource "aws_subnet" "private_subnet_1" {
-#   vpc_id = aws_vpc.example_vpc.id
-#   cidr_block = "10.0.3.0/24"
-#   availability_zone = "ap-southeast-1a"
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = aws_vpc.example_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-southeast-1a"
 
-#   tags = {
-#     Name = "private-subnet-1"
-#   }
-# }
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
 
-# resource "aws_subnet" "private_subnet_2" {
-#   vpc_id = aws_vpc.example_vpc.id
-#   cidr_block = "10.0.4.0/24"
-#   availability_zone = "ap-southeast-1b"
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id = aws_vpc.example_vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-southeast-1b"
 
-#   tags = {
-#     Name = "private-subnet-2"
-#   }
-# }
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
 
-# # Create an internet gateway and attach it to the VPC
-# resource "aws_internet_gateway" "example_igw" {
-#   vpc_id = aws_vpc.example_vpc.id
+# Create an internet gateway and attach it to the VPC
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
 
-#   tags = {
-#     Name = "example-igw"
-#   }
-# }
+  tags = {
+    Name = "example-igw"
+  }
+}
 
-# # Create a NAT gateway in the first public subnet and add an EIP
-# resource "aws_eip" "nat_eip" {
-#   vpc = true
+# Create a NAT gateway in the first public subnet and add an EIP
+resource "aws_eip" "nat_eip" {
+  vpc = true
 
-#   tags = {
-#     Name = "nat-eip"
-#   }
-# }
+  tags = {
+    Name = "nat-eip"
+  }
+}
 
-# resource "aws_nat_gateway" "example_nat_gateway" {
-#   allocation_id = aws_eip.nat_eip.id
-#   subnet_id = aws_subnet.public_subnet_1.id
+resource "aws_nat_gateway" "example_nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id = aws_subnet.public_subnet_1.id
 
-#   tags = {
-#     Name = "example-nat-gateway"
-#   }
-# }
+  tags = {
+    Name = "example-nat-gateway"
+  }
+}
 
-# # Create route tables for public and private subnets
-# resource "aws_route_table" "public_route_table" {
-#   vpc_id = aws_vpc.example_vpc.id
+# Create route tables for public and private subnets
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.example_igw.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
 
-#   tags = {
-#     Name = "public-route-table"
-#   }
-# }
+  tags = {
+    Name = "public-route-table"
+  }
+}
 
-# resource "aws_route_table" "private_route_table" {
-#   vpc_id = aws_vpc.example_vpc.id
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
 
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.example_nat_gateway.id
-#   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.example_nat_gateway.id
+  }
 
-#   tags = {
-#     Name = "private-route-table"
-#   }
-# }
+  tags = {
+    Name = "private-route-table"
+  }
+}
 
-# # Associate subnets with route tables
+# Associate subnets with route tables
