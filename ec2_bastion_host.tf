bdfe1ff26bab9811c4afdafee7767c3e98141223
tien.nguyen <tien.nguyen@kyanon.digital>
2023-04-24 16:20:05 +0700

    update new bastion

--- ec2_bastion_host.tf
+++ ec2_bastion_host.tf
@@ -1,64 +1,55 @@
-resource "aws_instance" "bastion_host" {
-  ami           = "ami-0a72af05d27b49ccb"
-  instance_type = "t2.micro"
-  key_name      = "aws_key"
-  subnet_id     = aws_subnet.public_subnet_1.id
-  vpc_security_group_ids = [aws_security_group.bastion.id]
-  user_data = "${data.template_file.userdata.rendered}"
-  associate_public_ip_address = true
-  tags = {
-    Name = "my-bastion-host"
-  }
-}
-
-
-# # Create a security group for the Bastion host
-# resource "aws_security_group" "bastion_security_group" {
-#   name_prefix = "bastion-sg-"
-#   vpc_id      = aws_vpc.example_vpc.id
-
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
+# resource "aws_instance" "bastion_host" {
+#   ami           = "ami-0a72af05d27b49ccb"
+#   instance_type = "t2.micro"
+#   key_name      = "aws_key"
+#   subnet_id     = aws_subnet.public_subnet_1.id
+#   vpc_security_group_ids = [aws_security_group.bastion.id]
+#   user_data = "${data.template_file.userdata.rendered}"
+#   associate_public_ip_address = true
+#   tags = {
+#     Name = "my-bastion-host"
 #   }
 # }
-resource "aws_security_group" "bastion" {
-#   name        = "Bastion host of ${local.project}"
-  name        = "Bastion host of"
 
-  description = "Allow SSH access to bastion host and outbound internet access"
-  vpc_id      = aws_vpc.example_vpc.id
 
-  lifecycle {
-    create_before_destroy = true
-  }
+# # # Create a security group for the Bastion host
+# # resource "aws_security_group" "bastion_security_group" {
+# #   name_prefix = "bastion-sg-"
+# #   vpc_id      = aws_vpc.example_vpc.id
 
-#   tags = {
-#     Project = local.project
+# #   ingress {
+# #     from_port   = 22
+# #     to_port     = 22
+# #     protocol    = "tcp"
+# #     cidr_blocks = ["0.0.0.0/0"]
+# #   }
+# # }
+# resource "aws_security_group" "bastion" {
+# #   name        = "Bastion host of ${local.project}"
+#   name        = "Bastion host of"
+
+#   description = "Allow SSH access to bastion host and outbound internet access"
+#   vpc_id      = aws_vpc.example_vpc.id
+
+#   lifecycle {
+#     create_before_destroy = true
 #   }
-}
 
-resource "aws_security_group_rule" "ssh" {
-  protocol          = "TCP"
-  from_port         = 22
-  to_port           = 22
-  type              = "ingress"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.bastion.id
-}
+# #   tags = {
+# #     Project = local.project
+# #   }
+# }
 
-resource "aws_security_group_rule" "internet" {
-  protocol          = "-1"
-  from_port         = 0
-  to_port           = 0
-  type              = "egress"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.bastion.id
-}
+# resource "aws_security_group_rule" "ssh" {
+#   protocol          = "TCP"
+#   from_port         = 22
+#   to_port           = 22
+#   type              = "ingress"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.bastion.id
+# }
 
-# resource "aws_security_group_rule" "intranet" {
+# resource "aws_security_group_rule" "internet" {
 #   protocol          = "-1"
 #   from_port         = 0
 #   to_port           = 0
@@ -66,3 +57,12 @@ resource "aws_security_group_rule" "internet" {
 #   cidr_blocks       = ["0.0.0.0/0"]
 #   security_group_id = aws_security_group.bastion.id
 # }
+
+# # resource "aws_security_group_rule" "intranet" {
+# #   protocol          = "-1"
+# #   from_port         = 0
+# #   to_port           = 0
+# #   type              = "egress"
+# #   cidr_blocks       = ["0.0.0.0/0"]
+# #   security_group_id = aws_security_group.bastion.id
+# # }
---
+++ ec2_bastion_host2.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "bastion-allow-ssh" {
+  vpc_id      = aws_vpc.example_vpc.id
+  name        = "bastion-allow-ssh"
+  description = "security group for bastion that allows ssh and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "bastion-allow-ssh"
+  }
+}
+
+resource "aws_security_group" "private-ssh" {
+  vpc_id      = aws_vpc.example_vpc.id
+  name        = "private-ssh"
+  description = "security group for private that allows ssh and all egress traffic"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [ aws_security_group.bastion-allow-ssh.id ]
+  }
+  tags = {
+    Name = "private-ssh"
+  }
+}
+
+
+resource "aws_instance" "bastion" {
+  ami           = ami-0a72af05d27b49ccb
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.bastion-allow-ssh.id]
+
+  key_name = aws_key
+}
+
+# resource "aws_instance" "private" {
+#   ami           = var.AMIS[var.AWS_REGION]
+#   instance_type = "t2.micro"
+#   subnet_id = aws_subnet.private.id
+#   vpc_security_group_ids = [aws_security_group.private-ssh.id]
+#   key_name = aws_key_pair.mykeypair.key_name
+# }
