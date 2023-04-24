d9fbfc06b3460f0a3504bbbcabc82a7795b106dc
tien.nguyen <tien.nguyen@kyanon.digital>
2023-04-24 13:57:33 +0700

    remove ec2

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-0a72af05d27b49ccb"
-  instance_type = "t2.micro"
-  key_name      = "aws_key"
-  subnet_id     = aws_subnet.private_subnet_1.id
-  vpc_security_group_ids = [aws_security_group.main.id]
-  user_data = "${data.template_file.userdata.rendered}"
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-0a72af05d27b49ccb"
+#   instance_type = "t2.micro"
+#   key_name      = "aws_key"
+#   subnet_id     = aws_subnet.private_subnet_1.id
+#   vpc_security_group_ids = [aws_security_group.main.id]
+#   user_data = "${data.template_file.userdata.rendered}"
   
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
+#   tags = {
+#     Name = "ExampleAppServerInstance"
+#   }
+# }
 
